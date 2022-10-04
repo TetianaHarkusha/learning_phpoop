94b953d40a31c67c544e104058b8d1c7801f688e
Tetiana <tatianagarkusha@gmail.com>
2022-10-04 19:04:55 +0200

    Created class Employee and changed file index.php

---
+++ task5/Models/Employee.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Task5;
+
+class Employee extends User
+{
+    
+    private float $salary;
+
+    public function __construct(string $name, int $age, float $salary)
+    {
+        parent::__construct($name, $age);
+        $this->salary = $salary;        
+    }
+    
+    public function getSalary(): float
+    {
+        return $this->salary;
+    }
+
+    public function checkAge(): bool
+    {
+        return ($this->age > 18)? true: false;
+    }
+
+    public function doubleSalary()
+    {
+        $this->salary = 2 * $this->salary;
+    }
+
+}
