a2a47d2d094286e2996cc2d544906c6fcf076ae8
Tetiana <tatianagarkusha@gmail.com>
2022-10-13 22:36:54 +0200

    Created class Student and changed file index.php in folder task8

---
+++ task8/Models/Student.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+namespace Task8;
+
+class Student
+{
+    const MAX_COURSE = 5;
+
+    private $name;
+    private $course = 1;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->$name)) {
+            return $this->$name;
+        }
+    }
+
+    public function transferToNextCourse(): void
+    {
+        if (self::isCourseCorrect(++$this->course)) {
+            $this->course;
+        } else {
+            unset($this->course);
+        }
+    }
+
+    private static function isCourseCorrect(int $course): bool
+    {
+        return $course <= self::MAX_COURSE;
+    }
+}
--- task8/index.php
+++ task8/index.php
@@ -12,3 +12,26 @@ foreach($employees as $employee)
 {
     echo $employee->getName() . ' ' . $employee->getSurname() . ' - ' . $employee->getSalary() . '<br>';
 }
+
+echo "<br> Об'єкти класу Student <br>";
+$students = [
+    new Task8\Student('Sarah'),
+    new Task8\Student('Ann'), 
+    new Task8\Student('Boris'),
+    new Task8\Student('Met'),
+    new Task8\Student('Honey')
+];
+for ($i = 1; $i <= 4; $i++) {
+    $students[2]->transferToNextCourse();
+}
+$students[4]->transferToNextCourse();
+foreach ($students as $student)
+{
+    echo $student->name . ' - ' . $student->course . ' курс<br>'; 
+} 
+echo"<br>Студенти переведені до наступного курсу: <br>";
+foreach ($students as $student)
+{
+    $student->transferToNextCourse();
+    echo $student->name . ' - ' . ($student->course ?? " закінчив навчання") . '<br>'; 
+} 
