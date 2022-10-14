4b9689428abc23df35524a7fcb8ffabc7a1eaf6a
TetianaHarkusha <114747376+TetianaHarkusha@users.noreply.github.com>
2022-10-14 16:53:46 +0200

    Updated class Employee, added comment in code

--- task8/Models/Employee.php
+++ task8/Models/Employee.php
@@ -8,6 +8,7 @@ class Employee
     private $surname;
     private $salary;
 
+    //construct
     public function __construct(string $name, string $surname, float $salary)
     {
         $this->name = $name;
@@ -15,21 +16,25 @@ class Employee
         $this->salary = $salary;
     }
 
+    //get method for property name
     public function getName(): string
     {
         return $this->name;
     }
 
+    //get method for property surname
     public function getSurname(): string
     {
         return $this->surname;
     }
 
+    //get method for property salary
     public function getSalary(): float
     {
         return $this->salary;
     }
 
+    //set method for property salary
     public function setSalary($salary): void
     {
         $this->salary = $salary;
