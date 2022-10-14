7d879825f371ba339c5eafa8789df306ff9b6688
TetianaHarkusha <114747376+TetianaHarkusha@users.noreply.github.com>
2022-10-14 16:46:32 +0200

    Updated class Student, added comment in code.

--- task8/Models/Student.php
+++ task8/Models/Student.php
@@ -4,16 +4,17 @@ namespace Task8;
 
 class Student
 {
-    const MAX_COURSE = 5;
+    const MAX_COURSE = 5; //maximum value for property course
 
     private $name;
-    private $course = 1;
+    private $course = 1; //default value 1
 
     public function __construct(string $name)
     {
         $this->name = $name;
     }
 
+    //magic method get for any property
     public function __get($name)
     {
         if (isset($this->$name)) {
@@ -21,6 +22,7 @@ class Student
         }
     }
 
+    //method for transferring a student to the next course
     public function transferToNextCourse(): void
     {
         if (!self::isCourseCorrect(++$this->course)) {
@@ -28,6 +30,7 @@ class Student
         }
     }
 
+    //static method for checking course
     private static function isCourseCorrect(int $course): bool
     {
         return $course <= self::MAX_COURSE;
