3fa436d73c454367439c7b9945f910bf470e5152
Tetiana <tatianagarkusha@gmail.com>
2022-10-21 15:01:03 +0200

    Created class City and updated file index.php in folder Task9

--- Task9/index.php
+++ Task9/index.php
@@ -8,3 +8,10 @@ echo "Створено об'єкт класу Arr <br>";
 
 //calculated the arithmetic mean of the numbers by method getAvg
 echo 'Середнє арифметичне чисел - ' . $arr->getAvg() . '<br><br>';
+
+//created objects of class City
+$city = new Task9\City('Полтава', '899р', 280000);
+echo "Створено об'єкт класу City: <br>";
+echo 'місто - ' . $city->name . '<br>'; 
+echo 'дата заснування - ' . $city->foundation . '<br>';
+echo 'населення - ' . $city->population . '<br>';
