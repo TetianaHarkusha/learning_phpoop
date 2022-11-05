39695827a0ecbfbbf72895afa83fcfa7ef34da3e
Tetiana <tatianagarkusha@gmail.com>
2022-11-05 20:35:21 +0100

    Task12, pp4-11. Changed class User and added Employee and Product

--- Task12/Models/User.php
+++ Task12/Models/User.php
@@ -9,11 +9,28 @@ use Exception;
 //The class for information about people, parent class
 class User
 {
-    const MIN_AGE = 18; //min value for property age
-    const MIN_NAME = 3; //min length for property name
+    public const MIN_BIRTHDAY = '1900-1-1'; //min value for property birthday
 
-    private string $name;
+    protected string $name;
+    protected string $surname;
+    private string $birthday;
     private int $age;
+
+    /**
+     * The constructor method for the class
+     * with initial values of properties name and surname
+     *
+     * @param string $birthday The date in format 'Y-m-d'
+     */
+    public function __construct(string $birthday)
+    {
+        $this->name = 'Іван'; //Initial values
+        $this->surname ='Іванов'; //Initial values
+        if ($this->isBirthdayCorrect($birthday)) {
+            $this->birthday = $birthday;
+            $this->age = $this->calculateAge($birthday);
+        }
+    }
 
     /**
      *  The method get for the property name
@@ -23,6 +40,26 @@ class User
     public function getName(): string
     {
         return $this->name;
+    }
+
+    /**
+     *  The method get for the property surname
+     *
+     * @return string
+     */
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    /**
+     *  The method get for the property birthday
+     *
+     * @return string
+     */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
     }
 
     /**
@@ -36,31 +73,60 @@ class User
     }
 
     /**
-     * The method set for the property name
+     *  The method to check a date
+     * (correct date format, validate a Gregorian date,
+     * more than MIN_BIRTHDAY
+     * and less than a current day)
      *
-     * @param string $name
-     * @return void
+     * @param string $birthday The date in format 'Y-m-d'
+     * @return string
      */
-    public function setName($name): void
+    private function isBirthdayCorrect(string $birthday): bool
     {
-        if (strlen($name) < self::MIN_NAME) {
-            throw new Exception('The length of the name value is too shot');
+        //a regular expression for a date format
+        $pattern = '#^[0-9]{4}-([0]?[1-9]|[1][0-2])-([0]?[1-9]|[1-2][0-9]|[3][0-1])$#';
+
+        $datePars = date_parse_from_format('Y-m-d', $birthday);
+
+        //checking for a date format
+        if (!preg_match($pattern, $birthday, $matches)) {
+            throw new Exception('Incorrect date format');
         }
-        $this->name = $name;
+
+        //checking for validate a Gregorian date
+        if (!checkdate($datePars['month'], $datePars['day'], $datePars['year'])) {
+            throw new Exception('No validate a Gregorian date');
+        }
+
+        //checking for range
+        if (strtotime($birthday) < strtotime(self::MIN_BIRTHDAY)) {
+            throw new Exception('Birthday is less than the minimum');
+        }
+        if (strtotime($birthday) > strtotime(date('Y-m-d'))) {
+            throw new Exception('Birthday is more than the current date');
+        }
+
+        return true;
     }
 
     /**
-     * The method set for the property age
+     *  The method of calculating age
+     *  by the date of birth on the current day
      *
-     * @param int $age
-     * @return void
+     * @param string $birthday The date in format 'Y-m-d'
+     * @return int
      */
-    public function setAge($age): void
+    private function calculateAge(string $birthday): int
     {
-        if ($age < self::MIN_AGE) {
-            throw new Exception('The value of age is too small');
+        $datPars = date_parse(date('Y-m-d'));
+        $birthPars = date_parse($birthday);
+        $age = $datPars['year'] - $birthPars['year'];
+        if ($datPars['month'] > $birthPars['month']) {
+            return $age;
         }
-        $this->age = $age;
+        if ($datPars['month'] = $birthPars['month'] and $datPars['day'] >= $birthPars['day']) {
+            return $age;
+        }
+        return --$age;
     }
-
 }
--- Task12/index.php
+++ Task12/index.php
@@ -3,16 +3,28 @@
 require_once __DIR__ . '/../vendor/autoload.php'; //connect autoload
 
 use Task12\User;
-use Task12\Student;
+use Task12\Employee;
+use Task12\Product;
 
-//created objects of class User and child classes Student
-$user = new User();
-$user->SetName('John');
-$user->setAge(22);
+//created objects of class User
+$user = new User('2001-12-10');
+echo "Створено об'єкт класу User: <br>" . $user->getName() . ' ' . $user->getSurname();
+echo ' (д.н. ' . $user->getBirthday() . ') - ' . $user->getAge() . ' рік/років(и). <br><br>';
 
-$student = new Student();
-$student->setName('Ann'); //the overridden parent method
-$student->setAge(19); //overridden parent method
+//created objects of class Employee
+$employee = new Employee('Ilon', 'Musk', '1971-06-28', 1000000);
+echo "Створено об'єкт класу Employee: <br>";
+echo $employee->getName() . ' ' . $employee->getSurname() . ' (д.н. ' . $employee->getBirthday() . ') - ';
+echo $employee->getAge() . ' рік/років(и), зарплата - ' . $employee->getSalary() . '.<br><br>';
 
-echo "Створено об'єкт класу User: " . $user->getName() . ', ' . $user->getAge() . 'р.<br>';
-echo "Створено об'єкт класу Student: " . $student->getName() . ', ' . $student->getAge() . 'р.<br>';
+//created objects of class Product
+echo "Створено об'єкт класу Product та змінні: <br>";
+$prod = new Product();
+$prod->name = 'bread';
+$prod->price = 19.5;
+$product1 = $prod;
+$product2 = $product1;
+echo 'Змінна product1:';
+var_dump($product1);
+echo '<br>Змінна product2:';
+var_dump($product2);
