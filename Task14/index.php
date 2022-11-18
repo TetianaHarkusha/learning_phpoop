df197619ecb43a3e466fa5f80bae959d809c0cce
Tetiana <tatianagarkusha@gmail.com>
2022-11-18 23:51:24 +0100

    Task14,pp1-3. Created class CompareHelper with methods for compare two objects.

--- Task14/index.php
+++ Task14/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php'; //connect autoload
 use Task14\Employee;
 use Task14\UsersCollection;
 use Task14\Student;
+use Task14\CompareHelper;
 
 //created objects of class UsersCollection and added objects of class Employee and Student to it
 $usersCollection = new UsersCollection();
@@ -22,4 +23,18 @@ print_r($usersCollection->getEmployees());
 //used method class UsersCollection
 echo '<br><br>Отримаємо повну суму стипендій: ' . $usersCollection->getTotalScholarship() . '<br>';
 echo 'Отримаємо повну суму зарплат: ' . $usersCollection->getTotalSalary() . '<br>';
-echo 'Отримаємо повну суму платежів: ' . $usersCollection->getTotalPayment() . '<br>';
+echo 'Отримаємо повну суму платежів: ' . $usersCollection->getTotalPayment() . '<br><br>';
+
+$helper = new CompareHelper();
+$obj1 = $usersCollection->getStudents()[0];
+$obj2 = new Student('Sarah', 300);
+
+var_dump($helper->compare1($obj1, $obj2));
+var_dump($helper->compare1($obj1, $usersCollection->getEmployees()[0]));
+
+var_dump($helper->compare2($obj1, $obj2));
+var_dump($helper->compare2($obj1, $usersCollection->getStudents()[0]));
+
+var_dump($helper->compare3($obj1, $obj2));
+var_dump($helper->compare3($obj1, $usersCollection->getStudents()[0]));
+var_dump($helper->compare3($obj1, new Student('Sarah', 400)));
