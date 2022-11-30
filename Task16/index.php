bb79a15eee8a0fe2b1fadfb85e155f64298eba56
Tetiana <tatianagarkusha@gmail.com>
2022-11-30 19:05:49 +0100

    Task16,pp3-4. Created class Circle with a constant

--- Task16/index.php
+++ Task16/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php'; //connect autoload
 
 use Task16\User;
+use Task16\Circle;
 
 //created and deleted objects of class User and printed count of objects
 $user1 = new User('John');
@@ -10,4 +11,9 @@ echo 'Count of object: ' . User::getCount() . "\n";
 $user2 = new User('Bob');
 echo 'Count of object: ' . $user2::getCount() . "\n";
 unset($user1);
-echo 'Count of object: ' . User::getCount() . "\n";
+echo 'Count of object: ' . User::getCount() . "\n\n";
+
+//created an object of class Circle
+$circle1 = new Circle(10);
+echo 'The area of a circle with a radius ' . $circle1->getRadius() . ' : '. $circle1->getSquare() . "\n";
+echo 'The circumference of a circle with a radius ' . $circle1->getRadius() . ' : '. $circle1->getСircuit() . "\n";
