49ba2b249c2411f6a9352a9a83d48e709498c598
Tetiana <tatianagarkusha@gmail.com>
2022-11-30 18:34:46 +0100

    Task16,p.2. Changed static property of class User to privat, adeed get-method

--- Task16/index.php
+++ Task16/index.php
@@ -6,8 +6,8 @@ use Task16\User;
 
 //created and deleted objects of class User and printed count of objects
 $user1 = new User('John');
-echo 'Count of object: ' . User::$count . "\n";
+echo 'Count of object: ' . User::getCount() . "\n";
 $user2 = new User('Bob');
-echo 'Count of object: ' . $user2::$count . "\n";
+echo 'Count of object: ' . $user2::getCount() . "\n";
 unset($user1);
-echo 'Count of object: ' . User::$count . "\n";
+echo 'Count of object: ' . User::getCount() . "\n";
