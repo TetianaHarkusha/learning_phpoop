610b7cbde42024cb0687870beed6009c18f46081
Tetiana <tatianagarkusha@gmail.com>
2022-12-20 17:21:53 +0100

    Tast18,pp1-2. Created interfaces iCube and class Cube.

--- Task18/index.php
+++ Task18/index.php
@@ -6,6 +6,7 @@ use Task18\Disk;
 use Task18\Quadrate;
 use Task18\FiguresCollection;
 use Task18\User;
+use Task18\Cube;
 
 //created objects of class FiguresCollection and added geometry figures
 $collection = new FiguresCollection();
@@ -21,3 +22,9 @@ $user1 = new User();
 $user1->setName('John');
 $user1->setAge(22);
 echo "\n \nСтворено об'єкт класу User: " . $user1->getName() . ', ' . $user1->getAge();
+
+//created objects of class Cube
+$cube1 = new Cube(3);
+echo "\n\nСтворено об'єкт класу Cube.\n";
+echo "об'єм куба: " . $cube1->getVolume() . "\n";
+echo "площі поверхні: " . $cube1->getSquareSurface();
