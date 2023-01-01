443a50c40dc50cdd1bf76c6e0027421a4d435ba1
Tetiana <tatianagarkusha@gmail.com>
2023-01-01 23:37:49 +0100

    Task19. Study functions.

--- Task19/index.php
+++ Task19/index.php
@@ -15,4 +15,10 @@ echo "населення: " . $country1->getPopulation();
 //created objects of classes Test
 $test1 = new Test();
 echo "\n\nСтворено об'єкт класу Test\n";
-echo 'getSum: ' . $test1->getSum();
+echo 'getSum: ' . $test1->getSum() . "\n";
+
+//study functions
+var_dump(trait_exists('Trait1'));
+var_dump(trait_exists('Task19\Trait1'));
+var_dump(trait_exists('Task19\Trait4'));
+var_dump(get_declared_traits());
